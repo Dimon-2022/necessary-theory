ce840f83bd5b855f2541c9f4871af3b644a542e6
dima.moisieienko <dimonmail09@gmail.com>
2025-05-11 15:58:02 +0300

    using useRef for accessing to elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,56 @@
-import React, { useState, useRef } from "react";
+// import React, { useState, useRef } from "react";
 
-function CounterApp() {
-  const [count, setCount] = useState(0);
+// function CounterApp() {
+//   const [count, setCount] = useState(0);
 
-  let clickCount = 0;
-  const clickRef = useRef(0);
-  console.log(clickRef);
+//   let clickCount = 0;
+//   const clickRef = useRef(0);
+//   console.log(clickRef);
 
-  const handleClick = () => {
-    setCount((prev) => prev + 1);
+//   const handleClick = () => {
+//     setCount((prev) => prev + 1);
 
-    clickCount++;
-    clickRef.current++;
-  };
+//     clickCount++;
+//     clickRef.current++;
+//   };
 
-  console.log("Component re-rendered");
+//   console.log("Component re-rendered");
+
+//   return (
+//     <div style={{ fontFamily: "Arial", padding: "20px" }}>
+//       <h1>Counter Example</h1>
+//       <p>Current Count (useState): {count}</p>
+//       <p>Total Clicks (variable): {clickRef.current}</p>
+
+//       <button
+//         onClick={handleClick}
+//         style={{
+//           padding: "10px 20px",
+//           fontSize: "16px",
+//           cursor: "pointer",
+//           marginTop: "10px",
+//         }}
+//       >
+//         Increment
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default CounterApp;
+
+import React, { useEffect, useRef } from "react";
+
+function App() {
+  const inputRef = useRef(null);
+
 
   return (
-    <div style={{ fontFamily: "Arial", padding: "20px" }}>
-      <h1>Counter Example</h1>
-      <p>Current Count (useState): {count}</p>
-      <p>Total Clicks (variable): {clickRef.current}</p>
-
-      <button
-        onClick={handleClick}
-        style={{
-          padding: "10px 20px",
-          fontSize: "16px",
-          cursor: "pointer",
-          marginTop: "10px",
-        }}
-      >
-        Increment
-      </button>
+    <div>
+      <input ref={inputRef} type="text" placeholder="Type something..." />
+      <button onClick={()=>{inputRef.current.focus()}}>Focus Input</button>
     </div>
   );
 }
 
-export default CounterApp;
+export default App;
